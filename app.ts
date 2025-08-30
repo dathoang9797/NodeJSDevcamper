9dad92257e668b996da5919d0ddfe79bfb3aff33
DESKTOP-9C48CIB\datho <dat>
2025-08-31 00:53:44 +0700

    up

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import routerBootcamp from "./Routers/Bootcamps.ts";
 
 dotenv.config({ path: './Config/Config.env' });
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use("/api/v1/bootcamps", routerBootcamp);
+
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);

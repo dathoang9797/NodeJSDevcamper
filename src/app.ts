a9e0316c78c152bee8630402e5cc3d1256296941
DESKTOP-9C48CIB\datho <dat>
2025-08-31 11:19:25 +0700

    up

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import routerBootcamp from "./Routers/Bootcamps.ts";
+import routerBootcamps from "#src/routers/bootcamps.ts";
 
-dotenv.config({ path: './Config/Config.env' });
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("/api/v1/bootcamps", routerBootcamp);
+app.use("/api/v1/bootcamps", routerBootcamps);
 
 app.use(express.json());
 app.use(morgan("dev"));

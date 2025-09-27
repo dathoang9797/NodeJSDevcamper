1ac9ba9e044d3736d91678004810110bdf8eb649
DESKTOP-9C48CIB\datho <dat>
2025-09-27 14:20:56 +0700

    up

--- dockerfile
+++ dockerfile
@@ -1,15 +1,13 @@
-# --- Build stage (cần dev deps để build)
 FROM node:24-alpine AS prod
 WORKDIR /app
 COPY package*.json ./
 RUN npm ci --omit=dev
 COPY . .
-RUN npm run build
+CMD ["npm","run","build"]
 
-# --- Runtime stage (prod nhẹ)
 FROM node:24-alpine AS dev
 WORKDIR /app
 COPY package*.json ./
 RUN npm ci
 COPY  . .
-RUN npm run build
+CMD ["npm","run","dev"]
--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const connectDB = async () => {
     try {
         console.log(process.env.MONGO_URL);
-        const conn = await mongoose.connect("mongodb://root:secret@mongo:27017/devcamper?authSource=admin");
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);

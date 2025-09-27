1d9ec52091926afc2b4f278263c4ea3f5f322d26
DESKTOP-9C48CIB\datho <dat>
2025-09-27 11:40:50 +0700

    up

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -2,7 +2,7 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         NODE_ENV: 'development' | 'test' | 'production' | 'staging';
-        MONGO_URI: string;
+        MONGO_URL: string;
         PORT?: `${number}`;
         JWT_EXPIRE: string;
         JWT_SECRET: string;
